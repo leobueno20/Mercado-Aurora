c5859db34045304ab2c92234e78d286c0c58ca5c
leobueno20 <109116969+leobueno20@users.noreply.github.com>
2024-08-06 15:03:10 -0300

    edit1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   purge: ['./src/**/*.html', './src/**/*.js'],
   darkMode: false, // ou 'media' ou 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'nome-da-fonte': ['Noto Sans', 'sans-serif'],
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      margin: ['responsive'],
+    },
   },
   plugins: [],
-}
+};
